0ab0eea75bf28956c9340762c3a2bbe97d5fdccb
Abdoulhakim89 <abdoulbeast89@gmail.com>
2026-02-05 11:28:05 +0200

    Change Cucumber tags from @AddToCart_ProductDetails to @All

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"steps","hooks","customerTypes"},
-        tags = "@AddToCart_ProductDetails",
+        tags = "@All",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
