9ca4638214483123ffd4e92fe72ae77589745a5d
Abdoulhakim89 <abdoulbeast89@gmail.com>
2026-02-05 11:57:18 +0200

    Made the driver headless

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -2,12 +2,15 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     private static WebDriver driver;
 
     public static WebDriver setUpDriver(){
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(
+                new ChromeOptions()
+                        .addArguments("--headless=new"));
         return driver;
     }
     public static WebDriver getDriver(){
